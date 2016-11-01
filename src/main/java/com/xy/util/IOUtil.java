f8350968e46d833c08a8c84c0036c4f299016058
xiuyang <jlss2011@163.com>
2016-11-01 18:12:02 +0800

    InputStream2String

--- src/main/java/com/xy/util/IOUtil.java
+++ src/main/java/com/xy/util/IOUtil.java
@@ -1,8 +1,11 @@
 package com.xy.util;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
 
 /**
  * Created by XiuYang on 2016/9/14.
@@ -10,7 +13,22 @@ import java.io.OutputStream;
 public class IOUtil {
 
     private static final int OUTPUT_BUFFER_SIZE = 4096;
+    private static final String DEFAULT_CHARSET = "UTF-8";
 
+    /**
+     * InputStream转String 完成之后关闭流
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public static String InputStream2String(InputStream inputStream,String charset) throws IOException {
+        Args.notNull(inputStream,"Input Stream");
+        try {
+            return IOUtils.toString(inputStream,Charset.forName(charset));
+        }finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+    }
     /**
      * input stream 写入 output stream
      * @param inputStream
@@ -18,8 +36,8 @@ public class IOUtil {
      * @throws IOException
      */
     public void write(InputStream inputStream, OutputStream outputStream) throws IOException {
-        Args.notNull(inputStream==null,"Input Stream");
-        Args.notNull(outputStream==null,"Output Stream");
+        Args.notNull(inputStream,"Input Stream");
+        Args.notNull(outputStream,"Output Stream");
         try{
             int l;
             final byte[] tmp = new byte[OUTPUT_BUFFER_SIZE];
